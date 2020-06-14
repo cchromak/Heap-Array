33c4a080a93fe97d66857c1571520875c3dde37c
cchromak <cchromak@gmail.com>
2020-06-14 12:44:02 -0400

    Added HeapArray class

--- src/com/company/HeapArray.java
+++ src/com/company/HeapArray.java
@@ -1,4 +1,51 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class HeapArray {
+
+    private int[] heap;
+    private int size;
+
+    public HeapArray(int capacity) {
+        heap = new int[capacity];
+    }
+
+    public void inset(int value) {
+        if(isFull()) {
+            throw new IndexOutOfBoundsException("FULL HEAP");
+        }
+        heap[size] = value;
+        bubbleUp(size);
+        size++;
+    }
+
+    private void bubbleUp(int index) {
+        int holdValue = heap[index];
+        while (index > 0 && holdValue > heap[getParent(index)]) {
+            heap[index] = heap[getParent(index)];
+            index = getParent(index);
+        }
+        heap[index] = holdValue;
+    }
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public int getParent(int index) {
+        return (index -1) / 2;
+    }
+
+    public int getLeftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    public int getRightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    public void printHeap() {
+        System.out.println(Arrays.toString(this.heap));
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    HeapArray heap = new HeapArray(10);
+
+	    heap.inset(1);
+        heap.inset(-43);
+        heap.inset(34);
+        heap.inset(67);
+        heap.inset(10);
+        heap.inset(-10);
+        heap.inset(34);
+
+        heap.printHeap();
+
     }
 }
